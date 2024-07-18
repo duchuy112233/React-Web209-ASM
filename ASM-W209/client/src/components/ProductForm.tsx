f241ba31fbd9763f9d42bd96ff1ac26129783fa5
hoangnamlee04 <150770315+hoangnamlee04@users.noreply.github.com>
2024-07-18 14:14:23 +0700

    sua form

--- ASM-W209/client/src/components/ProductForm.tsx
+++ ASM-W209/client/src/components/ProductForm.tsx
@@ -8,6 +8,9 @@ import {
   MenuItem,
   Select,
   Stack,
+  Typography,
+  Box,
+  Paper,
 } from "@mui/material";
 import { ValidationErrors } from "final-form";
 import { Field, Form } from "react-final-form";
@@ -23,106 +26,128 @@ function ProductForm({ onSubmit, initialValues }: ProductFormProps) {
   const validate = (values: ProductFormParams) => {
     const { title, image, category, price } = values;
     const errors: ValidationErrors = {};
-    if (!title) errors.title = "Can nhap title vao";
-    if (title && title.length < 6)
-      errors.title = "Can nhap toi thieu 6 ky tu vao";
-    if (!image) errors.image = "Can nhap image vao";
-    if (!category) errors.category = "Can nhap category vao";
-    if (!price) errors.price = "Can nhap price vao";
+    if (!title) errors.title = "Cần nhập tiêu đề";
+    if (title && title.length < 6) errors.title = "Cần nhập tối thiểu 6 ký tự";
+    if (!image) errors.image = "Cần nhập hình ảnh";
+    if (!category) errors.category = "Cần nhập danh mục";
+    if (!price) errors.price = "Cần nhập giá";
     return errors;
   };
 
   return (
-    <Form
-      onSubmit={onSubmit}
-      validate={validate}
-      initialValues={initialValues}
-      render={({ values }) => {
-        return (
-          <Stack>
-            <Field
-              name="title"
-              render={({ input, meta }) => (
-                <InputText
-                  input={input}
-                  label={"Title"}
-                  messageError={meta.touched && meta.error}
-                />
-              )}
-            />
-            <Field
-              name="image"
-              render={({ input, meta }) => (
-                <InputText
-                  input={input}
-                  label={"Image"}
-                  messageError={meta.touched && meta.error}
-                />
-              )}
-            />
-            <Field<string>
-              name="description"
-              render={({ input, meta }) => (
-                <InputText
-                  input={input}
-                  label={"Description"}
-                  messageError={meta.touched && meta.error}
-                />
-              )}
-            />
-            <Field<number>
-              name="price"
-              render={({ input, meta }) => (
-                <InputText
-                  input={input}
-                  label={"Price"}
-                  messageError={meta.touched && meta.error}
-                  type="number"
-                />
-              )}
-            />
-            <Field<string>
-              name="isShow"
-              type="checkbox"
-              render={({ input, meta }) => {
-                return (
-                  <FormControlLabel
-                    control={<Checkbox {...input} />}
-                    label="Show Product"
-                  />
-                );
-              }}
-            />
-            <Field<string>
-              name="category"
-              render={({ input, meta }) => {
-                return (
-                  <FormControl fullWidth>
-                    <InputLabel>Category</InputLabel>
-                    <Select label="Category" {...input} error>
-                      <MenuItem value="">Select</MenuItem>
-                      <MenuItem value={"66879becbac7f6bdb27eb74f"}>
-                        Thoi trang
-                      </MenuItem>
-                      <MenuItem value={"66879bf9bac7f6bdb27eb751"}>
-                        Phu Kien
-                      </MenuItem>
-                    </Select>
-                    {meta.touched && meta.error && (
-                      <FormHelperText>{meta.error}</FormHelperText>
-                    )}
-                  </FormControl>
-                );
-              }}
-            />
-
-            <Button type="submit" onClick={() => onSubmit(values)}>
-              Submit
-            </Button>
-          </Stack>
-        );
+    <Box
+      sx={{
+        maxWidth: "800px",
+        margin: "auto",
+        padding: "2rem",
+        width: "800px",
       }}
-    />
+    >
+      <Paper
+        elevation={4}
+        sx={{
+          padding: "2rem",
+          borderRadius: 2,
+        }}
+      >
+        <Typography variant="h4" align="center" gutterBottom></Typography>
+        <Form
+          onSubmit={onSubmit}
+          validate={validate}
+          initialValues={initialValues}
+          render={({ handleSubmit, submitting, pristine }) => (
+            <form onSubmit={handleSubmit}>
+              <Stack spacing={3}>
+                <Field
+                  name="title"
+                  render={({ input, meta }) => (
+                    <InputText
+                      input={input}
+                      label="Title"
+                      messageError={meta.touched && meta.error}
+                    />
+                  )}
+                />
+                <Field
+                  name="image"
+                  render={({ input, meta }) => (
+                    <InputText
+                      input={input}
+                      label="Image"
+                      messageError={meta.touched && meta.error}
+                    />
+                  )}
+                />
+                <Field<string>
+                  name="description"
+                  render={({ input, meta }) => (
+                    <InputText
+                      input={input}
+                      label="Description"
+                      messageError={meta.touched && meta.error}
+                    />
+                  )}
+                />
+                <Field<number>
+                  name="price"
+                  render={({ input, meta }) => (
+                    <InputText
+                      input={input}
+                      label="Price"
+                      messageError={meta.touched && meta.error}
+                      type="number"
+                    />
+                  )}
+                />
+                <Field<string>
+                  name="isShow"
+                  type="checkbox"
+                  render={({ input }) => (
+                    <FormControlLabel
+                      control={<Checkbox {...input} />}
+                      label="Show Product"
+                    />
+                  )}
+                />
+                <Field<string>
+                  name="category"
+                  render={({ input, meta }) => (
+                    <FormControl fullWidth error={meta.touched && !!meta.error}>
+                      <InputLabel>Category</InputLabel>
+                      <Select label="Category" {...input}>
+                        <MenuItem value="">Select</MenuItem>
+                        <MenuItem value={"669878459994fa10dc1b222f"}>
+                          Thời trang
+                        </MenuItem>
+                        <MenuItem value={"669878549994fa10dc1b2231"}>
+                          Phụ kiện
+                        </MenuItem>
+                      </Select>
+                      {meta.touched && meta.error && (
+                        <FormHelperText>{meta.error}</FormHelperText>
+                      )}
+                    </FormControl>
+                  )}
+                />
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  disabled={submitting || pristine}
+                  sx={{
+                    padding: "0.75rem",
+                    fontSize: "1rem",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Submit
+                </Button>
+              </Stack>
+            </form>
+          )}
+        />
+      </Paper>
+    </Box>
   );
 }
 
